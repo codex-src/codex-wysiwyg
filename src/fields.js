a20d87fb3febf439ba0c809de573b0ec04c885bf
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-06-09 19:31:02 +0900

    Created fields file with ASCII art -- should describe all allowable, possible combinations of consecutive fields

---
+++ src/fields.js
@@ -0,0 +1,79 @@
+// +-------+-------+
+// |       |\\\\\\\|
+// |   A   |\\\B\\\|
+// |       |\\\\\\\|
+// +-------+-------+
+//
+export function fieldsDoNotIntersect(fieldA, fieldB) {
+	return fieldB.offsetStart === fieldA.offsetEnd
+}
+
+//   +---+---+---+
+//   |   |\\\|\\\|
+//   | A |\\\B\\\|
+//   |   |\\\|\\\|
+//   +---+---+---+
+//
+export function fieldsArePartiallyIntersected(fieldA, fieldB) {
+	const ok = (
+		fieldB.offsetStart < fieldA.offsetEnd &&
+		fieldB.offsetEnd > fieldA.offsetEnd
+	)
+	return ok
+}
+
+//     +-------+
+//     |\\\\\\\|
+//     |\\\B\\\| A
+//     |\\\\\\\|
+//     +-------+
+//
+export function fieldsAreTotallyIntersected(fieldA, fieldB) {
+	const ok = (
+		fieldB.offsetStart === fieldA.offsetStart &&
+		fieldB.offsetEnd === fieldA.offsetEnd
+	)
+	return ok
+}
+
+//     +---+---+
+//     |   |\\\|
+//     | A |\B\|
+//     |   |\\\|
+//     +---+---+
+//
+export function fieldIsContainedRHS(fieldA, fieldB) {
+	const ok = (
+		fieldB.offsetStart < fieldA.offsetStart &&
+		fieldB.offsetEnd === fieldA.offsetEnd
+	)
+	return ok
+}
+
+//     +-+---+-+
+//     | |\\\| |
+//     | |\B\| | A
+//     | |\\\| |
+//     +-+---+-+
+//
+export function fieldIsTotallyContained(fieldA, fieldB) {
+	const ok = (
+		fieldB.offsetStart > fieldA.offsetStart &&
+		fieldB.offsetEnd < fieldA.offsetEnd
+	)
+	return ok
+}
+
+//     +---+---+
+//     |\\\|   |
+//     |\B\| A |
+//     |\\\|   |
+//     +---+---+
+//
+export function fieldIsContainedLHS(fieldA, fieldB) {
+	const ok = (
+		fieldB.offsetStart === fieldA.offsetStart &&
+		fieldB.offsetEnd < fieldA.offsetEnd
+	)
+	return ok
+}
