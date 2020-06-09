152cbbbaa7e1aa465a06dee368902f2992e7e45a
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-06-09 20:01:05 +0900

    Simplified internals -- note nothing renders to screen right now because new the new intersection functions have not yet been integrated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,15 @@
 import React from "react"
+import uuidv4 from "uuid/v4"
+
+import {
+	Code,
+	Em,
+	Header,
+	Paragraph,
+	Strike,
+	Strong,
+	Unstyled,
+} from "./components"
 
 import {
 	fieldIsContained,
@@ -8,80 +19,6 @@ import {
 	fieldsAreTotallyIntersected,
 	fieldsDoNotIntersect,
 } from "./fields"
-
-const Header = ({ children }) => (
-	<h1 className="">
-		{children || (
-			<br />
-		)}
-	</h1>
-)
-
-const Paragraph = ({ children }) => (
-	<p className="">
-		{children || (
-			<br />
-		)}
-	</p>
-)
-
-const Unstyled = ({ children }) => (
-	children
-)
-
-const Em = ({ children }) => (
-	<em className="!bg-blue-50 !rounded">
-		{children}
-	</em>
-)
-
-const Strong = ({ children }) => (
-	<strong className="!bg-blue-50 !rounded">
-		{children}
-	</strong>
-)
-
-const Code = ({ children }) => (
-	<code className="px-1 py-1 text-sm font-mono text-blue-500 bg-gray-100 rounded">
-		{children}
-	</code>
-)
-
-const Strike = ({ children }) => (
-	<strike className="line-through text-gray-300">
-		{children}
-	</strike>
-)
-
-// Returns whether an inline element field is nested.
-function fieldIsNested(fieldA, fieldB) {
-	const ok = (
-		fieldB.offsetStart >= fieldA.offsetStart &&
-		fieldB.offsetEnd <= fieldA.offsetEnd
-	)
-	return ok
-}
-
-// Returns whether an inline element field is partially
-// nested.
-function fieldIsPartiallyNested(fieldA, fieldB) {
-	const ok = (
-		fieldB.offsetStart < fieldA.offsetEnd &&
-		fieldB.offsetEnd > fieldA.offsetEnd
-	)
-	return ok
-}
-
-// Retursn whether an inline element field is nested or
-// partially nested.
-function fieldIsNestedOrPartiallyNested(fieldA, fieldB) {
-	const ok = (
-		fieldIsNested(fieldA, fieldB) ||
-		fieldIsPartiallyNested(fieldA, fieldB)
-	)
-	return ok
-}
-
 
 // Returns the deepest reference to props.children.
 function deepestElement(element) {
@@ -94,8 +31,11 @@ function deepestElement(element) {
 	return lastRef
 }
 
-// Prepares renderable React components.
-function toReact(components, elementMap) {
+// Converts elements to renderable React components.
+//
+// TODO: Deprecate ... null || ... "string" case because of
+// <Unstyled>?
+function toReact(components, componentMap) {
 	if (components === null || typeof components === "string") {
 		return components
 	}
@@ -106,156 +46,163 @@ function toReact(components, elementMap) {
 			continue
 		}
 		const { type: T, props } = each
-		const Element = elementMap[T]
-		renderable.push((
-			<Element key={renderable.length} {...props}>
-				{toReact([props.children], elementMap)}
-			</Element>
-		))
+		const Element = componentMap[T]
+		// TODO: Argument components must be an array; uses
+		// [...] for now
+		renderable.push(React.createElement(componentMap[T], {
+			key: renderable.length,
+			...props,
+		}, toReact([props.children], componentMap)))
 	}
 	return renderable
 }
 
-// TODO: Add comment
-function parseComponents(block, elementMap) {
+// Parses an element from a VDOM node.
+function parseElements(nodes, componentMap) {
 	const components = []
-	for (let x = 0; x < block.fields.length; x++) {
+	for (let x = 0; x < nodes.fields.length; x++) {
 
-		// if (x && block.fields[x].offsetStart === block.fields[x - 1].offsetStart && block.fields[x].offsetEnd === block.fields[x - 1].offsetEnd) {
-		// 	const ref = deepestElement(components[components.length - 1])
-		// 	ref.props.children = {
-		// 		type: block.fields[x].type,
-		// 		props: {
-		// 			children: ref.props.children,
-		// 		},
-		// 	}
-
-		if (x && fieldIsNested(block.fields[x - 1], block.fields[x])) {
-			// TODO: Three cases: slice LHS, no slice, slice RHS
-			const ref = deepestElement(components[components.length - 1])
-			const offsetStart = block.fields[x].offsetStart - block.fields[x - 1].offsetStart
-			const offsetEnd = offsetStart + block.fields[x].offsetEnd - block.fields[x].offsetStart
-			console.log(
-				ref.props.children.slice(0, offsetStart),
-				ref.props.children.slice(offsetStart, offsetEnd),
-				ref.props.children.slice(offsetEnd),
-			)
-			ref.props.children = {
-				type: block.fields[x].type,
-				props: {
-					children: ref.props.children,
-				},
-			}
-
-			// const A = elementMap[block.fields[x].type]
-			// const B = elementMap[block.fields[x + 1].type]
-			// children.push((
-			// 	<A key={children.length}>
-			// 		{block.text.slice(block.fields[x].offsetStart,
-			// 			block.fields[x + 1].offsetStart)}
-			// 		<B>
-			// 			{block.text.slice(block.fields[x + 1].offsetStart,
-			// 				block.fields[x + 1].offsetEnd)}
-			// 		</B>
-			// 		{block.text.slice(block.fields[x + 1].offsetEnd,
-			// 			block.fields[x].offsetEnd)}
-			// 	</A>
-			// ))
-			// x++
-
-		// } else if (x && fieldIsPartiallyNested(block.fields[x], block.fields[x - 1])) {
-			// ...
-		} else {
-			components.push({
-				type: block.fields[x].type,
-				props: {
-					children: block.text.slice(block.fields[x].offsetStart, block.fields[x].offsetEnd),
-				},
-			})
-		}
-
-		// if (x + 1 < block.fields.length && fieldIsNested(block.fields[x], block.fields[x + 1])) {
-		// 	const A = elementMap[block.fields[x].type]
-		// 	const B = elementMap[block.fields[x + 1].type]
-		// 	children.push((
-		// 		<A key={children.length}>
-		// 			{block.text.slice(block.fields[x].offsetStart,
-		// 				block.fields[x + 1].offsetStart)}
-		// 			<B>
-		// 				{block.text.slice(block.fields[x + 1].offsetStart,
-		// 					block.fields[x + 1].offsetEnd)}
-		// 			</B>
-		// 			{block.text.slice(block.fields[x + 1].offsetEnd,
-		// 				block.fields[x].offsetEnd)}
-		// 		</A>
-		// 	))
-		// 	x++
-		// } else if (x + 1 < block.fields.length && fieldIsPartiallyNested(block.fields[x], block.fields[x + 1])) {
-		// 	const A = elementMap[block.fields[x].type]
-		// 	const B = elementMap[block.fields[x + 1].type]
-		// 	children.push((
-		// 		<React.Fragment key={children.length}>
-		// 			<A key={children.length}>
-		// 				{block.text.slice(block.fields[x].offsetStart,
-		// 					block.fields[x + 1].offsetStart)}
-		// 				<B key={children.length}>
-		// 					{block.text.slice(block.fields[x + 1].offsetStart,
-		// 						block.fields[x].offsetEnd)}
-		// 				</B>
-		// 			</A>
-		// 			<B key={children.length}>
-		// 				{block.text.slice(block.fields[x].offsetEnd,
-		// 					block.fields[x + 1].offsetEnd)}
-		// 			</B>
-		// 		</React.Fragment>
-		// 	))
-		// 	x++
-		// } else {
-		// 	const A = elementMap[block.fields[x].type]
-		// 	children.push((
-		// 		<A key={children.length}>
-		// 			{block.text.slice(block.fields[x].offsetStart,
-		// 				block.fields[x].offsetEnd)}
-		// 		</A>
-		// 	))
-		// }
+		//		// if (x && nodes.fields[x].offsetStart === nodes.fields[x - 1].offsetStart && nodes.fields[x].offsetEnd === nodes.fields[x - 1].offsetEnd) {
+		//		// 	const ref = deepestElement(components[components.length - 1])
+		//		// 	ref.props.children = {
+		//		// 		type: nodes.fields[x].type,
+		//		// 		props: {
+		//		// 			children: ref.props.children,
+		//		// 		},
+		//		// 	}
+		//
+		//		if (x && fieldIsNested(nodes.fields[x - 1], nodes.fields[x])) {
+		//			// TODO: Three cases: slice LHS, no slice, slice RHS
+		//			const ref = deepestElement(components[components.length - 1])
+		//			const offsetStart = nodes.fields[x].offsetStart - nodes.fields[x - 1].offsetStart
+		//			const offsetEnd = offsetStart + nodes.fields[x].offsetEnd - nodes.fields[x].offsetStart
+		//			console.log(
+		//				ref.props.children.slice(0, offsetStart),
+		//				ref.props.children.slice(offsetStart, offsetEnd),
+		//				ref.props.children.slice(offsetEnd),
+		//			)
+		//			ref.props.children = {
+		//				type: nodes.fields[x].type,
+		//				props: {
+		//					children: ref.props.children,
+		//				},
+		//			}
+		//
+		//			// const A = componentMap[nodes.fields[x].type]
+		//			// const B = componentMap[nodes.fields[x + 1].type]
+		//			// children.push((
+		//			// 	<A key={children.length}>
+		//			// 		{nodes.text.slice(nodes.fields[x].offsetStart,
+		//			// 			nodes.fields[x + 1].offsetStart)}
+		//			// 		<B>
+		//			// 			{nodes.text.slice(nodes.fields[x + 1].offsetStart,
+		//			// 				nodes.fields[x + 1].offsetEnd)}
+		//			// 		</B>
+		//			// 		{nodes.text.slice(nodes.fields[x + 1].offsetEnd,
+		//			// 			nodes.fields[x].offsetEnd)}
+		//			// 	</A>
+		//			// ))
+		//			// x++
+		//
+		//		// } else if (x && fieldIsPartiallyNested(nodes.fields[x], nodes.fields[x - 1])) {
+		//			// ...
+		//		} else {
+		//			components.push({
+		//				type: nodes.fields[x].type,
+		//				props: {
+		//					children: nodes.text.slice(nodes.fields[x].offsetStart, nodes.fields[x].offsetEnd),
+		//				},
+		//			})
+		//		}
+		//
+		//		// if (x + 1 < nodes.fields.length && fieldIsNested(nodes.fields[x], nodes.fields[x + 1])) {
+		//		// 	const A = componentMap[nodes.fields[x].type]
+		//		// 	const B = componentMap[nodes.fields[x + 1].type]
+		//		// 	children.push((
+		//		// 		<A key={children.length}>
+		//		// 			{nodes.text.slice(nodes.fields[x].offsetStart,
+		//		// 				nodes.fields[x + 1].offsetStart)}
+		//		// 			<B>
+		//		// 				{nodes.text.slice(nodes.fields[x + 1].offsetStart,
+		//		// 					nodes.fields[x + 1].offsetEnd)}
+		//		// 			</B>
+		//		// 			{nodes.text.slice(nodes.fields[x + 1].offsetEnd,
+		//		// 				nodes.fields[x].offsetEnd)}
+		//		// 		</A>
+		//		// 	))
+		//		// 	x++
+		//		// } else if (x + 1 < nodes.fields.length && fieldIsPartiallyNested(nodes.fields[x], nodes.fields[x + 1])) {
+		//		// 	const A = componentMap[nodes.fields[x].type]
+		//		// 	const B = componentMap[nodes.fields[x + 1].type]
+		//		// 	children.push((
+		//		// 		<React.Fragment key={children.length}>
+		//		// 			<A key={children.length}>
+		//		// 				{nodes.text.slice(nodes.fields[x].offsetStart,
+		//		// 					nodes.fields[x + 1].offsetStart)}
+		//		// 				<B key={children.length}>
+		//		// 					{nodes.text.slice(nodes.fields[x + 1].offsetStart,
+		//		// 						nodes.fields[x].offsetEnd)}
+		//		// 				</B>
+		//		// 			</A>
+		//		// 			<B key={children.length}>
+		//		// 				{nodes.text.slice(nodes.fields[x].offsetEnd,
+		//		// 					nodes.fields[x + 1].offsetEnd)}
+		//		// 			</B>
+		//		// 		</React.Fragment>
+		//		// 	))
+		//		// 	x++
+		//		// } else {
+		//		// 	const A = componentMap[nodes.fields[x].type]
+		//		// 	children.push((
+		//		// 		<A key={children.length}>
+		//		// 			{nodes.text.slice(nodes.fields[x].offsetStart,
+		//		// 				nodes.fields[x].offsetEnd)}
+		//		// 		</A>
+		//		// 	))
+		//		// }
 
 	}
 
 	return components
 }
 
-
 const CodexEditor = ({
-	header,
-	paragraph,
-	unstyled,
-	em,
-	strong,
-	code,
-	strike,
-	...props
-}) => {
-
-	// Maps block element types (strings) to renderable block
-	// components.
-	//
-	// TODO: Change to useMemo? useMemo(() => { ... }, [])
-	const elementMap = {
+	components: {
 		header,
 		paragraph,
-
 		unstyled,
 		em,
 		strong,
 		code,
 		strike,
-	}
+	},
+	...props
+}) => {
+	// Maps element types (strings) to renderable React
+	// components.
+	const componentMap = React.useMemo(() => ({
+		header,
+		paragraph,
+		unstyled,
+		em,
+		strong,
+		code,
+		strike,
+	}), [
+		header,
+		paragraph,
+		unstyled,
+		em,
+		strong,
+		code,
+		strike,
+	])
 
-	const blocks = [
+	// TODO: Move to useState or equivalent
+	const nodes = [
 		{
 			type: "paragraph",
-			key: "TODO:uuidv4()",
+			key: uuidv4(),
 			text: "Hello, world! How are you?",
 			fields: [
 				{
@@ -287,18 +234,18 @@ const CodexEditor = ({
 					type: "em",
 					offsetStart: 18,
 					offsetEnd: 21,
-				}
+				},
 			],
 		},
 	]
 
 	return (
 		<article>
-			{blocks.map(({ type: T, ...each }) => {
-				const parsedComponents = parseComponents(each, elementMap)
-				return React.createElement(elementMap[T], {
+			{nodes.map(({ type: T, ...each }) => {
+				const elements = parseElements(each, componentMap)
+				return React.createElement(componentMap[T], {
 					key: each.key,
-				}, toReact(parsedComponents, elementMap))
+				}, toReact(elements, componentMap))
 			})}
 		</article>
 	)
@@ -308,13 +255,15 @@ const App = () => (
 	<div className="px-6 py-24 flex flex-row justify-center">
 		<div className="w-full max-w-3xl">
 			<CodexEditor
-				header={Header}
-				paragraph={Paragraph}
-				unstyled={Unstyled}
-				em={Em}
-				strong={Strong}
-				code={Code}
-				strike={Strike}
+				components={{
+					header: Header,
+					paragraph: Paragraph,
+					unstyled: Unstyled,
+					em: Em,
+					strong: Strong,
+					code: Code,
+					strike: Strike,
+				}}
 			/>
 		</div>
 	</div>
---
+++ src/components/index.js
@@ -0,0 +1,47 @@
+import React from "react"
+
+// TODO: Use React.memo?
+export const Header = ({ children }) => (
+	<h1 className="">
+		{children || (
+			<br />
+		)}
+	</h1>
+)
+
+// TODO: Use React.memo?
+export const Paragraph = ({ children }) => (
+	<p className="">
+		{children || (
+			<br />
+		)}
+	</p>
+)
+
+export const Unstyled = ({ children }) => (
+	children
+)
+
+export const Em = ({ children }) => (
+	<em className="italic">
+		{children}
+	</em>
+)
+
+export const Strong = ({ children }) => (
+	<strong className="font-bold">
+		{children}
+	</strong>
+)
+
+export const Code = ({ children }) => (
+	<code className="px-1 py-1 text-sm font-mono text-blue-500 bg-gray-100 rounded">
+		{children}
+	</code>
+)
+
+export const Strike = ({ children }) => (
+	<strike className="line-through text-gray-300">
+		{children}
+	</strike>
+)
