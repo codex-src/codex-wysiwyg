5fe2fca86dbc5e689c79daab9d1827f8427893e2
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-06-11 09:55:13 +0900

    Changed semantic inline elements to spans for now (it may be preferable to not rely on semantic elements at all for the read-write editor because there are side effects to discover)

--- src/components/index.js
+++ src/components/index.js
@@ -34,36 +34,35 @@ export const Paragraph = ({ children }) => (
 )
 
 export const Emphasis = ({ children }) => (
-	<em className="italic">
+	<span className="italic">
 		{children}
-	</em>
+	</span>
 )
 
 export const Strong = ({ children }) => (
-	// NOTE: Uses font-semibold not font-bold
-	<strong className="font-semibold">
+	<span className="font-semibold">
 		{children}
-	</strong>
+	</span>
 )
 
 const codeClassNames = {
-	undefined:   "px-1 py-1 text-sm font-mono text-blue-500 bg-gray-100 rounded",
+	"undefined": "px-1 py-1 text-sm font-mono text-blue-500 bg-gray-100 rounded",
 	"at-start":  "pl-1 py-1 text-sm font-mono text-blue-500 bg-gray-100 rounded-l",
 	"at-center": "px-0 py-1 text-sm font-mono text-blue-500 bg-gray-100 rounded-none",
 	"at-end":    "pr-1 py-1 text-sm font-mono text-blue-500 bg-gray-100 rounded-r",
 }
 
 export const Code = ({ typePos, children }) => (
-	<code className={codeClassNames[typePos]} {...disableAutoCorrect}>
+	<span className={codeClassNames[typePos]} {...disableAutoCorrect}>
 		{children}
-	</code>
+	</span>
 )
 
 export const Strikethrough = ({ children }) => (
 	// NOTE: Uses text-gray-400 not text-gray-500
-	<strike className="line-through text-gray-400">
+	<span className="line-through text-gray-400">
 		{children}
-	</strike>
+	</span>
 )
 
 export const Anchor = ({ href, children }) => (
