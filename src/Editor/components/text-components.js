d3b2209e155709ff109a5e014e09b826c2e51a5a
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-06-19 23:22:58 +0900

    Added <Type> decorator to text-components

--- src/Editor/components/text-components.js
+++ src/Editor/components/text-components.js
@@ -1,32 +1,51 @@
 import attrs from "./attrs"
 import React from "react"
+import { typeEnum } from "./typeMaps"
+
+// TODO: Remove? We can read node.nodeName
+const Type = ({ type: T, children }) => (
+	React.cloneElement(children, {
+		"data-type": T,
+		...children.props,
+	})
+)
 
 export const Em = ({ children }) => (
-	<em className="italic">
-		{children}
-	</em>
+	<Type type={typeEnum.em}>
+		<em className="italic">
+			{children}
+		</em>
+	</Type>
 )
 
 export const Strong = ({ children }) => (
-	<strong className="font-bold">
-		{children}
-	</strong>
+	<Type type={typeEnum.strong}>
+		<strong className="font-bold">
+			{children}
+		</strong>
+	</Type>
 )
 
 export const Code = ({ children }) => (
-	<code className="px-1 py-0.5 text-sm font-mono text-blue-600 border border-cool-gray-300 rounded" {...attrs.code}>
-		{children}
-	</code>
+	<Type type={typeEnum.code}>
+		<code className="px-1 py-0.5 text-sm font-mono text-blue-600 border border-cool-gray-300 rounded" {...attrs.code}>
+			{children}
+		</code>
+	</Type>
 )
 
 export const Strike = ({ children }) => (
-	<strike className="line-through text-gray-400">
-		{children}
-	</strike>
+	<Type type={typeEnum.strike}>
+		<strike className="line-through text-gray-400">
+			{children}
+		</strike>
+	</Type>
 )
 
 export const A = ({ href, children }) => (
-	<a className="mx-px underline text-blue-600" href={href} {...attrs.a}>
-		{children}
-	</a>
+	<Type type={typeEnum.a}>
+		<a className="mx-px underline text-blue-600" href={href} {...attrs.a}>
+			{children}
+		</a>
+	</Type>
 )
