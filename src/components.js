644cbef67c6527f23c313e6521701effacb39f0d
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-06-28 05:19:30 +0900

    Implemented new Node API for rendering spans as outside of React

--- src/components.js
+++ src/components.js
@@ -2,15 +2,10 @@ import React from "react"
 import ReactDOMServer from "react-dom/server"
 import syncDOM from "./syncDOM"
 import toReact from "./toReact"
+import types from "./types"
 
 const T = ({ type, props, children }) => (
 	React.cloneElement(children, {
-		style: {
-			// https://github.com/codex-src/codex-wysiwyg/commit/f0755661d24e900804ab43b9657ec584c00bbbca
-			...children.props.style, // Takes precedence
-			whiteSpace: "pre-wrap",
-			overflowWrap: "break-word",
-		},
 		// Enum type:
 		"data-type": type,
 		// JSON-encoded props:
@@ -18,50 +13,52 @@ const T = ({ type, props, children }) => (
 	})
 )
 
-// Renders HTML text to HTML.
-function toHTML(text_html) {
-	const fragment = document.createDocumentFragment()
-	const doc = new DOMParser().parseFromString(text_html, "text/html")
-	fragment.append(...doc.body.childNodes)
-	return fragment
-}
+const Node = React.forwardRef(({ id, style, children }, ref) => {
 
-function useDOMRenderer(ref, children) {
+	// NOTE: children are not managed by React.
+	// TODO: Reimplement syncDOM for inline elements
 	React.useLayoutEffect(() => {
 		if (!ref.current) {
 			// No-op
 			return
 		}
+
+		// Renders markup to HTML.
+		function markupToHTML(text_html) { // TODO
+			const fragment = document.createDocumentFragment()
+			const doc = new DOMParser().parseFromString(text_html, "text/html")
+			fragment.append(...doc.body.childNodes)
+			return fragment
+		}
+
 		const markup = ReactDOMServer.renderToStaticMarkup(children)
-		const fragment = toHTML(markup)
+		const fragment = markupToHTML(markup)
 		// TODO
 		;[...ref.current.childNodes].reverse().map(each => each.remove())
 		ref.current.append(...fragment.childNodes)
 	}, [ref, children])
-}
 
-// const DOMRenderer = ({ forwardedRef, children }) => {
-// 	React.useLayoutEffect(() => {
-// 		if (!forwardedRef.current) {
-// 			// No-op
-// 			return
-// 		}
-// 		const markup = ReactDOMServer.renderToStaticMarkup(children)
-// 		const fragment = toHTML(markup)
-// 		// TODO
-// 		;[...forwardedRef.current.childNodes].reverse().map(each => each.remove())
-// 		forwardedRef.current.append(...fragment.childNodes)
-// 	}, [forwardedRef, children])
-// 	return null
-// }
+	return React.createElement("div", {
+		ref,
+		id,
+		style: {
+			// https://github.com/codex-src/codex-wysiwyg/commit/f0755661d24e900804ab43b9657ec584c00bbbca
+			...style, // Takes precedence
+			whiteSpace: "pre-wrap",
+			overflowWrap: "break-word",
+		},
+	})
+})
 
-export const P = React.memo(({ type, id, spans }) => {
+export const P = React.memo(({ id, spans }) => {
 	const ref = React.useRef(null)
-	useDOMRenderer(ref, toReact(spans) || <br />)
-
 	return (
-		<T type={type}>
-			<div ref={ref} id={id} />
+		<T type={types.p}>
+			<Node ref={ref} id={id}>
+				{toReact(spans) || (
+					<br />
+				)}
+			</Node>
 		</T>
 	)
 })
