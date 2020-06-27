53978e61726999493e7dade48d6f5866010a34ac
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-06-28 03:50:46 +0900

    Fixed an issue with syncDOM; syncDOM previously only iterated elements. In theory, changing children to childNodes should yield the same effect, though this is not heavily tested outside of the previous test suite.

--- src/syncDOM.js
+++ src/syncDOM.js
@@ -6,35 +6,35 @@ function syncDOM(src, dst) {
 	// Iterate forwards (before replaceWith):
 	let mutations = 0
 	let start = 0
-	const min = Math.min(dst.children.length, src.children.length)
+	const min = Math.min(dst.childNodes.length, src.childNodes.length)
 	for (; start < min; start++) {
-		if (!dst.children[start].isEqualNode(src.children[start])) {
-			const clonedElement = src.children[start].cloneNode(true)
-			dst.children[start].replaceWith(clonedElement)
+		if (!dst.childNodes[start].isEqualNode(src.childNodes[start])) {
+			const clonedElement = src.childNodes[start].cloneNode(true)
+			dst.childNodes[start].replaceWith(clonedElement)
 			mutations++
 			start++ // Eagerly increment
 			break
 		}
 	}
 	// Iterate backwards (after replaceWith):
-	let end1 = dst.children.length
-	let end2 = src.children.length
+	let end1 = dst.childNodes.length
+	let end2 = src.childNodes.length
 	for (; end1 > start && end2 > start; end1--, end2--) {
-		if (!dst.children[end1 - 1].isEqualNode(src.children[end2 - 1])) {
-			const clonedElement = src.children[end2 - 1].cloneNode(true)
-			dst.children[end1 - 1].replaceWith(clonedElement)
+		if (!dst.childNodes[end1 - 1].isEqualNode(src.childNodes[end2 - 1])) {
+			const clonedElement = src.childNodes[end2 - 1].cloneNode(true)
+			dst.childNodes[end1 - 1].replaceWith(clonedElement)
 			mutations++
 		}
 	}
 	if (start < end1) {
 		for (; start < end1; end1--) { // Iterate backwards
-			dst.children[end1 - 1].remove()
+			dst.childNodes[end1 - 1].remove()
 			mutations++
 		}
 	} else if (start < end2) {
 		for (; start < end2; start++) {
-			const clonedElement = src.children[start].cloneNode(true)
-			dst.insertBefore(clonedElement, dst.children[start])
+			const clonedElement = src.childNodes[start].cloneNode(true)
+			dst.insertBefore(clonedElement, dst.childNodes[start])
 			mutations++
 		}
 	}
