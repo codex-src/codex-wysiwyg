dea51d7f4b51cb73949ea3a5b64f136a307da505
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-07-01 05:51:10 +0900

    Fixed a bug in deeplySyncDOMTrees; do not recurse on non-DOM elements

--- src/Editor/deeplySyncDOMTrees/deeplySyncDOMTrees.js
+++ src/Editor/deeplySyncDOMTrees/deeplySyncDOMTrees.js
@@ -1,10 +1,10 @@
+import domUtils from "lib/domUtils"
 import shallowlySyncDOMNodes from "./shallowlySyncDOMNodes"
 
-// Deeply synchronizes DOM trees; synchronizes start-to-end,
-// then removes or ends end-to-start.
+// Deeply synchronizes DOM trees.
 function deeplySyncDOMTrees(src, dst, __internalRecursionCount = 0) {
 	if (__internalRecursionCount) {
-		// NOTE: shallowlySyncDOMNodes **does not** sync
+		// NOTE: shallowlySyncDOMNodes **does not** synchronize
 		// domNode.childNodes.
 		shallowlySyncDOMNodes(src, dst)
 		if (src.isEqualNode(dst)) {
@@ -14,15 +14,19 @@ function deeplySyncDOMTrees(src, dst, __internalRecursionCount = 0) {
 	}
 	const x2 = Math.min(src.childNodes.length, dst.childNodes.length)
 	for (let x1 = 0; x1 < x2; x1++) {
-		deeplySyncDOMTrees(src.childNodes[x1], dst.childNodes[x1], __internalRecursionCount + 1)
+		let sync = shallowlySyncDOMNodes
+		if (domUtils.isElement(src.childNodes[x1]) && domUtils.isElement(dst.childNodes[x1])) {
+			sync = (src, dst) => deeplySyncDOMTrees(src, dst, __internalRecursionCount + 1)
+		}
+		sync(src.childNodes[x1], dst.childNodes[x1])
 	}
-	// Remove extraneous end-to-x2:
+	// Remove extraneous end-to-start:
 	if (x2 < dst.childNodes.length) {
 		const domNodes = [...dst.childNodes].slice(x2).reverse()
 		for (const each of domNodes) {
 			each.remove()
 		}
-	// Append extraneous end-to-x2:
+	// Append extraneous end-to-start:
 	} else if (x2 < src.childNodes.length) {
 		const domNodes = [...src.childNodes].slice(x2).map(each => each.cloneNode(true))
 		for (const each of domNodes) {
