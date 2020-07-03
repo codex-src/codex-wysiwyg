4aece57321a50e6bf62b76795b75aeff890bb97c
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-07-03 09:33:47 +0900

    Better comments

--- src/lib/JSONClone/index.js
+++ src/lib/JSONClone/index.js
@@ -1,4 +1,6 @@
-// JSON-encodes and decodes a value.
+// JSON-encodes and decodes a value; not recommended for
+// deeply nested objects because serializing and
+// deserializing a value has O(n^2) time complexity.
 function JSONClone(value) {
 	return JSON.parse(JSON.stringify(value))
 }
