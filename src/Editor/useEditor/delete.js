c22ab4e84fa543828f885ee73f343daf36d4d2e6
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-07-04 20:58:25 +0900

    More simplification

--- src/Editor/useEditor/delete.js
+++ src/Editor/useEditor/delete.js
@@ -3,7 +3,34 @@ import JSONClone from "lib/JSONClone"
 import methods from "./methods"
 import queryCollection from "./queryCollection"
 
-// Deletes a boundary (in a direction) on the current range.
+// // Deletes right-to-left on the current range.
+// const deleteRTL = state => () => {
+// 	const collection = queryCollection(state)
+// 	for (const c of collection) {
+// 		const [s1, s2] = c.offsets.spans
+// 		c.refs.element.props.spans.splice(s1, s2 - s1)
+// 	}
+// 	if (collection.length > 1) {
+// 		const x1 = collection[0].offsets.element
+// 		const x2 = collection[collection.length - 1].offsets.element
+// 		state.elements.splice(x1, (x2 - x1) + 1, {
+// 			...state.elements[x1],
+// 			props: {
+// 				...state.elements[x1].props,
+// 				spans: [
+// 					...state.elements[x1].props.spans,
+// 					...state.elements[x2].props.spans,
+// 				],
+// 			},
+// 		})
+// 	}
+// 	const collapsed = Range.collapse(state.range)
+// 	methods(state).select(collapsed)
+// 	methods(state).render()
+// }
+
+// Deletes on the current range. If the cursor is collapsed,
+// a synthetic range is computed based on dir and boundary.
 //
 // TODO: Remove empty spans
 const $delete = state => (dir, boundary) => {
@@ -13,23 +40,21 @@ const $delete = state => (dir, boundary) => {
 		const [s1, s2] = c.offsets.spans
 		c.refs.element.props.spans.splice(s1, s2 - s1)
 	}
-
 	if (collection.length > 1) {
 		const x1 = collection[0].offsets.element
 		const x2 = collection[collection.length - 1].offsets.element
 		state.elements.splice(x1, (x2 - x1) + 1, {
-			...collection[0].refs.element,
+			...state.elements[x1],
 			props: {
-				...collection[0].refs.element,
+				...state.elements[x1].props,
 				spans: [
-					...collection[0].ref.props.spans,
-					...collection[collection.length - 1].ref.props.spans,
+					...state.elements[x1].props.spans,
+					...state.elements[x2].props.spans,
 				],
 			},
 		})
 	}
 
-	// Done -- render:
 	const collapsed = Range.collapse(state.range)
 	methods(state).select(collapsed)
 	methods(state).render()
