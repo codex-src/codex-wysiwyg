b2fb0b96a3829c92a6938c8cffe9e3b215094e44
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-07-10 06:54:45 +0900

    Better comments

--- src/Editor/model/Editor/InlineElement.js
+++ src/Editor/model/Editor/InlineElement.js
@@ -1,15 +1,21 @@
 import { immerable } from "immer"
 
-// Describes an inline element, such as plaintext or rich
-// text. An inline element is considered discerete when
-// types and props are not equal.
+// Describes an inline element; an inline element is a
+// representation of zero-to-many nested inline elements,
+// such as plaintext or <strong><em>.
+//
+// Note that there is only one correct representation of an
+// inline element because types are sortable.
+//
 class InlineElement {
 	[immerable] = true
 
 	// Zero-to-many types.
 	types = []
+
 	// Zero-to-many props.
 	props = {}
+
 	// Character data.
 	value = ""
 
