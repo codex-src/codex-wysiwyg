cfeb7f5b12eefbb4a4566926e5c33dff4415130c
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-07-17 04:00:54 +0900

    Added applyFormat test suite

---
+++ src/RichTextEditor/utils/keyDownTypeFor/applyFormat.test.js
@@ -0,0 +1,117 @@
+import applyFormat from "./applyFormat"
+import keyCodeFor from "lib/Client/keyCodeFor"
+
+import { // Unsorted
+	mockNonMacOS,
+	mockMacOS,
+} from "lib/Client/mockUserAgent"
+
+/*
+ * em
+ */
+test("em(...); non-macOS", () => {
+	mockNonMacOS()
+	expect(applyFormat.em({
+		shiftKey: false,
+		ctrlKey: true,
+		metaKey: false,
+		keyCode: keyCodeFor("I"),
+	})).toBeTruthy()
+})
+test("em(...); macOS", () => {
+	mockMacOS()
+	expect(applyFormat.em({
+		shiftKey: false,
+		ctrlKey: false,
+		metaKey: true,
+		keyCode: keyCodeFor("I"),
+	})).toBeTruthy()
+})
+
+/*
+ * strong
+ */
+test("strong(...); non-macOS", () => {
+	mockNonMacOS()
+	expect(applyFormat.strong({
+		shiftKey: false,
+		ctrlKey: true,
+		metaKey: false,
+		keyCode: keyCodeFor("B"),
+	})).toBeTruthy()
+})
+test("strong(...); macOS", () => {
+	mockMacOS()
+	expect(applyFormat.strong({
+		shiftKey: false,
+		ctrlKey: false,
+		metaKey: true,
+		keyCode: keyCodeFor("B"),
+	})).toBeTruthy()
+})
+
+/*
+ * code
+ */
+test("code(...); non-macOS", () => {
+	mockNonMacOS()
+	expect(applyFormat.code({
+		shiftKey: true,
+		ctrlKey: true,
+		metaKey: false,
+		keyCode: keyCodeFor("C"),
+	})).toBeTruthy()
+})
+test("code(...); macOS", () => {
+	mockMacOS()
+	expect(applyFormat.code({
+		shiftKey: true,
+		ctrlKey: false,
+		metaKey: true,
+		keyCode: keyCodeFor("C"),
+	})).toBeTruthy()
+})
+
+/*
+ * strike
+ */
+test("strike(...); non-macOS", () => {
+	mockNonMacOS()
+	expect(applyFormat.strike({
+		shiftKey: true,
+		ctrlKey: true,
+		metaKey: false,
+		keyCode: keyCodeFor("X"),
+	})).toBeTruthy()
+})
+test("strike(...); macOS", () => {
+	mockMacOS()
+	expect(applyFormat.strike({
+		shiftKey: true,
+		ctrlKey: false,
+		metaKey: true,
+		keyCode: keyCodeFor("X"),
+	})).toBeTruthy()
+})
+
+/*
+ * a
+ */
+test("a(...); non-macOS", () => {
+	mockNonMacOS()
+	expect(applyFormat.a({
+		shiftKey: false,
+		ctrlKey: true,
+		metaKey: false,
+		keyCode: keyCodeFor("K"),
+	})).toBeTruthy()
+})
+test("a(...); macOS", () => {
+	mockMacOS()
+	expect(applyFormat.a({
+		shiftKey: false,
+		ctrlKey: false,
+		metaKey: true,
+		keyCode: keyCodeFor("K"),
+	})).toBeTruthy()
+})
