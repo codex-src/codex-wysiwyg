3fd6a3ca8c5f75d61f64982ca7d6732881b43f40
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-07-17 03:49:28 +0900

    Added test suite for applyFormatMarkdown

---
+++ src/RichTextEditor/utils/keyDownTypeFor/applyFormatMarkdown.test.js
@@ -0,0 +1,56 @@
+import applyFormatMarkdown from "./applyFormatMarkdown"
+import keyCodeFor from "lib/Client/keyCodeFor"
+
+test("em(...)", () => {
+	expect(applyFormatMarkdown.em({
+		shiftKey: true,
+		ctrlKey: false,
+		metaKey: false,
+		keyCode: keyCodeFor("_"),
+	})).toBeTruthy()
+})
+
+test("strong(...)", () => {
+	expect(applyFormatMarkdown.strong({
+		shiftKey: true,
+		ctrlKey: false,
+		metaKey: false,
+		keyCode: keyCodeFor("*"),
+	})).toBeTruthy()
+})
+
+test("code(...)", () => {
+	expect(applyFormatMarkdown.code({
+		shiftKey: false,
+		ctrlKey: false,
+		metaKey: false,
+		keyCode: keyCodeFor("`"),
+	})).toBeTruthy()
+})
+
+test("strike(...)", () => {
+	expect(applyFormatMarkdown.strike({
+		shiftKey: true,
+		ctrlKey: false,
+		metaKey: false,
+		keyCode: keyCodeFor("~"),
+	})).toBeTruthy()
+})
+
+test("a(...)", () => {
+	expect(applyFormatMarkdown.a({
+		shiftKey: false,
+		ctrlKey: false,
+		metaKey: false,
+		keyCode: keyCodeFor("["),
+	})).toBeTruthy()
+})
+
+test("a(...)", () => {
+	expect(applyFormatMarkdown.a({
+		shiftKey: false,
+		ctrlKey: false,
+		metaKey: false,
+		keyCode: keyCodeFor("]"),
+	})).toBeTruthy()
+})
