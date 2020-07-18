d71ba0b353b22a4e7a0b9ae03ac9162d2e0c8645
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-07-18 12:42:45 +0900

    Fixed bugs with split. Essentially, we had a typo and we also need to guard offset=0 and offset=each.props.children.length.

--- src/RichTextEditor/utils/split/index.js
+++ src/RichTextEditor/utils/split/index.js
@@ -1,77 +1,39 @@
 import JSONClone from "lib/JSON/JSONClone"
 
-// Reads text content.
-function textContent(children) {
-	return children.reduce((acc, each) => acc += each, "")
-}
-
 // Splits (mutates) children at an offset.
 function split(children, offset) {
+	// Must guard offset=0 (at start):
 	if (!offset) {
 		return 0
-	} // else if (offset === -1) {
-	// 	return children.length
-	// }
+	}
 	let x = 0
 	let each = null
 	for ([x, each] of children.entries()) {
 		if (offset - each.props.children.length <= 0) {
-			// No-op
 			break
 		}
 		offset -= each.props.children.length
 	}
-	// Return the next offset:
+	// Must guard offset=each.props.children.length (at end):
 	if (offset === each.props.children.length) {
 		return x + 1
 	}
-	// Cut the current span and return the next offset:
 	const current = {
 		...JSONClone(each),
 		props: {
+			...each.props,
 			children: each.props.children.slice(0, offset),
 		},
 	}
 	const next = {
 		...JSONClone(each),
 		props: {
+			...each.props,
 			children: each.props.children.slice(offset),
 		},
 	}
 	children.splice(x, 1, current, next)
 	return x + 1
 }
-
-	// if (!offset) {
-	// 	return 0
-	// } else if (offset === textContent(children).length) {
-	// 	return children.length
-	// }
-	// let x = 0
-	// let each = null
-	// for ([x, each] of children.entries()) {
-	// 	if (offset - each.props.children.length <= 0) {
-	// 		if (offset === each.props.children.length) {
-	// 			return x + 1
-	// 		}
-	// 		break
-	// 	}
-	// 	offset -= each.props.children.length
-	// }
-	// const current = {
-	// 	...JSONClone(each),
-	// 	props: {
-	// 		children: each.props.children.slice(0, offset),
-	// 	},
-	// }
-	// const next = {
-	// 	...JSONClone(each),
-	// 	props: {
-	// 		children: each.props.children.slice(offset),
-	// 	},
-	// }
-	// children.splice(x, 1, current, next)
-	// return x + 1
-// }
 
 export default split
