98e19c725f85726e1e24b4c3c2257298edcf0d7f
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-07-18 12:53:52 +0900

    All delete methods appear to be working (untested)

--- src/RichTextEditor/methods/ElementList/index.js
+++ src/RichTextEditor/methods/ElementList/index.js
@@ -3,7 +3,7 @@ import split from "../../utils/split"
 const initialState = {
 	prev: null,     // The previous element link
 	elements: null, // The current elements
-	x: 0,           // The current element offset
+	index: 0,       // The current element index
 	current: null,  // The current element
 	next: null,     // The next element link
 }
@@ -15,7 +15,7 @@ export function fromElements(elements) {
 	for (const each of elements) {
 		Object.assign(ref, {
 			elements,
-			x: !ref.prev ? 0 : ref.prev.x + 1,
+			index: !ref.prev ? 0 : ref.prev.index + 1,
 			current: each,
 			next: {
 				...initialState,
@@ -48,6 +48,11 @@ export const findKey = list => key => {
 	return find(list)(each => each.key === key)
 }
 
+// k1.current.props.children = [
+// 	...k1.current.props.children.slice(0, x1),
+// 	...k2.current.props.children.slice(x2),
+// ]
+
 // Deletes a range.
 export const deleteRange = list => range => {
 	const k1 = findKey(list)(range.start.key)
@@ -55,30 +60,19 @@ export const deleteRange = list => range => {
 	if (!range.collapsed()) {
 		k2 = findKey(list)(range.end.key)
 	}
-	// Keys are the same:
 	const x1 = split(k1.current.props.children, range.start.offset)
 	const x2 = split(k2.current.props.children, range.end.offset)
-	if (k1 === k2) {
-		// console.log({ range: JSON.stringify(range) })
-		console.log({ x1, x2 })
-		k1.current.props.children.splice(x1, x2 - x1)
-		return
+	k1.current.props.children.splice(
+		0,
+		k1.current.props.children.length,
+		...k1.current.props.children.slice(0, x1),
+		...k2.current.props.children.slice(x2),
+	)
+	let k = k2
+	while (k !== k1) {
+		k.elements.splice(k.index, 1)
+		k = k.prev
 	}
-	// const x1 = split(k1.current.props.children, range.start.offset)
-	// const x2 = split(k2.current.props.children, range.end.offset)
-	// k1.current.props.children = [...k1.current.props.children.slice(0, x1), ...k2.current.props.children.slice(x2)]
-	// // Keys are the same:
-	// if (k1 === k2) {
-	// 	// No-op
-	// 	return
-	// }
-	// // Keys are **not** the same:
-	// k2.elements.splice(k2.x, 1) // Takes precedence
-	// let k = k2.prev
-	// while (k !== k1) {
-	// 	k.elements.splice(k.x, 1)
-	// 	k = k.prev
-	// }
 }
 
 // // Formats a range.
