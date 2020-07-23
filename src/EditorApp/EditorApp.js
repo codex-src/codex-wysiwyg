f029c28308acd036759f36dd86c3a67419590ffd
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-07-23 10:21:21 +0900

    Extracted ReleaseItem

--- src/EditorApp/EditorApp.js
+++ src/EditorApp/EditorApp.js
@@ -42,6 +42,21 @@ const classNameMap = {
 	html: "prism-custom-theme",
 }
 
+const ReleaseItem = ({ date, release, children }) => (
+	<div className="px-6 py-4">
+		<div className="pb-2 flex flex-row justify-end">
+			<div className="px-2.5 py-0.5 bg-green-100 rounded-full">
+				<p className="font-medium text-xs leading-4 text-green-800">
+					{date} &nbsp;– &nbsp;v{release}
+				</p>
+			</div>
+		</div>
+		<ReadOnlyEditor className="text-gray-800" style={{ fontSize: "0.9375rem" }}>
+			{children}
+		</ReadOnlyEditor>
+	</div>
+)
+
 const Output = React.forwardRef(({ output, setOutput }, forwardedRef) => {
 	const debouncedElements = React.useContext(ElementsContext)
 
@@ -77,124 +92,48 @@ const Output = React.forwardRef(({ output, setOutput }, forwardedRef) => {
 				{output.detail === "changelog" ? (
 
 					<React.Fragment>
-						<div className="px-6 py-4">
-							<div className="pb-2 flex flex-row justify-end">
-								<div className="px-2.5 py-0.5 bg-green-100 rounded-full">
-									<p className="font-medium text-xs leading-4 text-green-800">
-										July 22, 2020 &nbsp;– &nbsp;v0.3
-									</p>
-								</div>
-							</div>
-							<ReadOnlyEditor className="text-gray-800" style={{ fontSize: "0.9375rem" }}>
-								<p>
-									– Added a dedicated changelog and improved readability for markdown and HTML-rendered code.
-								</p>
-							</ReadOnlyEditor>
-						</div>
+						<ReleaseItem date="July 22, 2020" release="0.3">
+							<p>
+								– Added a dedicated changelog and improved readability for markdown and HTML-rendered code.
+							</p>
+						</ReleaseItem>
 						<hr className="border-t border-gray-100" />
-						<div className="px-6 py-4">
-							<div className="pb-2 flex flex-row justify-end">
-								<div className="px-2.5 py-0.5 bg-green-100 rounded-full">
-									<p className="font-medium text-xs leading-4 text-green-800">
-										July 21, 2020 &nbsp;– &nbsp;v0.2
-									</p>
-								</div>
-							</div>
-							<ReadOnlyEditor className="text-gray-800" style={{ fontSize: "0.9375rem" }}>
-								<p>
-									– Added basic support for resolving to plaintext, GitHub-Flavored Markdown, and HTML.
-								</p>
-							</ReadOnlyEditor>
-						</div>
+						<ReleaseItem date="July 21, 2020" release="0.2">
+							<p>
+								– Added basic support for resolving to plaintext, GitHub-Flavored Markdown, and HTML.
+							</p>
+						</ReleaseItem>
 						<hr className="border-t border-gray-100" />
-						<div className="px-6 py-4">
-							<div className="pb-2 flex flex-row justify-end">
-								<div className="px-2.5 py-0.5 bg-green-100 rounded-full">
-									<p className="font-medium text-xs leading-4 text-green-800">
-										July 20, 2020 &nbsp;– &nbsp;v0.1
-									</p>
-								</div>
-							</div>
-							<ReadOnlyEditor className="text-gray-800" style={{ fontSize: "0.9375rem" }}>
-								<p>
-									– Added complete support for all backspace operations, such as <code>"backspace-rune"</code>, <code>"backspace-word"</code>, and <code>"backspace-line"</code>.
-								</p>
-								<p>
-									<br />
-								</p>
-								<p>
-									– Added basic support for <em>italics</em>, <strong>bold</strong>, <code>code</code>, <strike>strikethrough</strike>, and <a href="TODO">link</a> for inline elements. Of course, elements can be <em><strong>nested</strong></em> if that’s your thing.
-								</p>
-								<p>
-									<br />
-								</p>
-								<p>
-									– Added basic support for shortcuts. You can use <code>{ctrlOrCmd}-i</code> for <em>italics</em>, <code>{ctrlOrCmd}-b</code> for <strong>bold</strong>, <code>shift-{ctrlOrCmd}-c</code> for <code>code</code>, <code>shift-{ctrlOrCmd}-x</code> for <strike>strikethrough</strike>, and <code>{ctrlOrCmd}-k</code> for <a href="TODO">links</a>. Finally, you can use <code>shift-{ctrlOrCmd}-p</code> to <em>remove</em> formatting from a selection.
-								</p>
-								<p>
-									<br />
-								</p>
-								<p>
-									*Note that formatting without a selection is not yet supported.
-								</p>
-							</ReadOnlyEditor>
-						</div>
+						<ReleaseItem date="July 20, 2020" release="0.1">
+							<p>
+								– Added complete support for all backspace operations, such as <code>"backspace-rune"</code>, <code>"backspace-word"</code>, and <code>"backspace-line"</code>.
+							</p>
+							<p>
+								<br />
+							</p>
+							<p>
+								– Added basic support for <em>italics</em>, <strong>bold</strong>, <code>code</code>, <strike>strikethrough</strike>, and <a href="TODO">link</a> for inline elements.{" "}
+								Of course, elements can be <em><strong>nested</strong></em> if that’s your thing.
+							</p>
+							<p>
+								<br />
+							</p>
+							<p>
+								– Added basic support for shortcuts.{" "}
+								You can use <code>{ctrlOrCmd}-i</code> for <em>italics</em>, <code>{ctrlOrCmd}-b</code> for <strong>bold</strong>, <code>shift-{ctrlOrCmd}-c</code> for <code>code</code>, <code>shift-{ctrlOrCmd}-x</code> for <strike>strikethrough</strike>, and <code>{ctrlOrCmd}-k</code> for <a href="TODO">links</a>.{" "}
+								Finally, you can use <code>shift-{ctrlOrCmd}-p</code> to <em>remove</em> formatting from a selection.
+							</p>
+							<p>
+								<br />
+							</p>
+							<p>
+								*Note that formatting without a selection is not yet supported.
+							</p>
+						</ReleaseItem>
 					</React.Fragment>
 
-				// <ReadOnlyEditor>
-				// 	<p>
-				// 		<strong>v0.3: July 22, 2020</strong>
-				// 	</p>
-				// 	<p>
-				// 		<br />
-				// 	</p>
-				// 	<p>
-				// 		– Added a changelog and improved the readabiliy for markdown-rendered and HTML-rendered code.
-				// 	</p>
-				// 	<p>
-				// 		<br />
-				// 	</p>
-				// 	<p>
-				// 		<strong>v0.2: July 21, 2020</strong>
-				// 	</p>
-				// 	<p>
-				// 		<br />
-				// 	</p>
-				// 	<p>
-				// 		– Added basic support for resolving to plaintext, GitHub-Flavored Markdown, and HTML.
-				// 	</p>
-				// 	<p>
-				// 		<br />
-				// 	</p>
-				// 	<p>
-				// 		<strong>v0.1: July 20, 2020</strong>
-				// 	</p>
-				// 	<p>
-				// 		<br />
-				// 	</p>
-				// 	<p>
-				// 		– Added complete support for all backspace operations, such as <code>"backspace-rune"</code>, <code>"backspace-word"</code>, and <code>"backspace-line"</code>.
-				// 	</p>
-				// 	<p>
-				// 		<br />
-				// 	</p>
-				// 	<p>
-				// 		– Added basic support for <em>italics</em>, <strong>bold</strong>, <code>code</code>, <strike>strikethrough</strike>, and <a href="TODO">link</a> for elements. Of course, elements can be <em><strong>nested</strong></em> if that’s your thing.
-				// 	</p>
-				// 	<p>
-				// 		<br />
-				// 	</p>
-				// 	<p>
-				// 		– Added basic support for shortcuts. You can use <code>{ctrlOrCmd}-i</code> for <em>italics</em>, <code>{ctrlOrCmd}-b</code> for <em>bold</em>, <code>shift-{ctrlOrCmd}-c</code> for <em>code</em>, <code>shift-{ctrlOrCmd}-x</code> for <strike>strikethrough</strike>, and <code>{ctrlOrCmd}-k</code> for <a href="TODO">links</a>. Finally, you can use <code>shift-{ctrlOrCmd}-p</code> to <em>remove</em> formatting from a selection.
-				// 	</p>
-				// 	<p>
-				// 		<br />
-				// 	</p>
-				// 	<p>
-				// 		<em>*Note that formatting without a selection is not yet supported.</em>
-				// 	</p>
-				// </ReadOnlyEditor>
 				) : (
+
 					<div className="p-6">
 						<span className="inline-block">
 							<pre className="whitespace-pre-wrap break-words font-mono text-gray-800 subpixel-antialiased" style={{ MozTabSize: 2, tabSize: 2, fontSize: "0.8125rem", lineHeight: 1.4375 }}>
@@ -204,6 +143,7 @@ const Output = React.forwardRef(({ output, setOutput }, forwardedRef) => {
 							</pre>
 						</span>
 					</div>
+
 				)}
 			</div>
 		</Transition>
