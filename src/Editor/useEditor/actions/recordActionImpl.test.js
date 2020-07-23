152a4638fa086052fa97e2acc09d1df747d939cc
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-07-23 23:17:44 +0900

    Added recordActionImpl test suite

--- src/Editor/useEditor/actions/recordActionImpl.test.js
+++ src/Editor/useEditor/actions/recordActionImpl.test.js
@@ -1,3 +1,4 @@
+import sleepFor from "lib/x/sleepFor"
 import { default as record } from "./recordActionImpl"
 
 const e = {
@@ -5,23 +6,19 @@ const e = {
 	lastAction: "init",
 }
 
-// https://stackoverflow.com/a/39914235
-function sleep(ms) {
-	new Promise(r => setTimeout(r, 2000))
-}
-
 test("record(...)", async () => {
 	expect(e.lastAction).toBe("init")
 
 	record(e)("focus")
 	expect(e.lastAction).toBe("focus")
+
 	record(e)("blur")
 	expect(e.lastAction).toBe("blur")
 
 	record(e)("select")
 	expect(e.lastAction).toBe("blur") // No-op
 
-	await sleep(1e3)
+	await sleepFor(1e3)
 	record(e)("select")
 	expect(e.lastAction).toBe("select")
 })
