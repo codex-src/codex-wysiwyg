f6c7a3df4aabbb91e1b6561481f7ec69a18cee89
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-07-23 11:51:07 +0900

    Extracted Releases module

--- src/EditorApp/EditorApp.js
+++ src/EditorApp/EditorApp.js
@@ -1,10 +1,10 @@
 import * as handlers from "lib/x/handlers"
 import content from "./content"
-import ctrlOrCmd from "lib/Client/ctrlOrCmd"
 import DebugCSS from "lib/x/DebugCSS"
 import Highlight from "lib/PrismJS/Highlight"
 import keyCodeFor from "lib/Client/keyCodeFor"
 import React from "react"
+import Releases from "./Releases"
 import tmpl from "lib/x/tmpl"
 import Transition from "lib/x/Transition"
 
@@ -15,24 +15,8 @@ import { // Unsorted
 
 import { // Unsorted
 	Editor,
-	ReadOnlyEditor,
 	useEditorFromChildren,
 } from "Editor"
-
-const ReleaseItem = ({ date, release, children }) => (
-	<div className="px-6 py-4">
-		<div className="pb-2 flex flex-row justify-end">
-			<div className="px-2.5 py-0.5 bg-green-100 rounded-full">
-				<p className="font-medium text-xs leading-4 text-green-800">
-					{date} &nbsp;– &nbsp;v{release}
-				</p>
-			</div>
-		</div>
-		<ReadOnlyEditor className="text-gray-800" style={{ fontSize: "0.9375rem" }}>
-			{children}
-		</ReadOnlyEditor>
-	</div>
-)
 
 const Output = React.forwardRef(({ output, setOutput }, forwardedRef) => {
 	const debouncedElements = React.useContext(ElementsContext)
@@ -71,50 +55,8 @@ const Output = React.forwardRef(({ output, setOutput }, forwardedRef) => {
 		>
 			<div ref={forwardedRef} className="w-full max-w-lg max-h-full bg-white rounded-lg shadow-hero-lg overflow-y-scroll">
 				{output.detail === "changelog" ? (
-
-					<React.Fragment>
-						<ReleaseItem date="July 22, 2020" release="0.3">
-							<p>
-								– Added a dedicated changelog and improved readability for markdown and HTML-rendered code.
-							</p>
-						</ReleaseItem>
-						<hr className="border-t border-gray-100" />
-						<ReleaseItem date="July 21, 2020" release="0.2">
-							<p>
-								– Added basic support for resolving to plaintext, GitHub-Flavored Markdown, and HTML.
-							</p>
-						</ReleaseItem>
-						<hr className="border-t border-gray-100" />
-						<ReleaseItem date="July 20, 2020" release="0.1">
-							<p>
-								– Added complete support for all backspace operations, such as <code>"backspace-rune"</code>, <code>"backspace-word"</code>, and <code>"backspace-line"</code>.
-							</p>
-							<p>
-								<br />
-							</p>
-							<p>
-								– Added basic support for <em>italics</em>, <strong>bold</strong>, <code>code</code>, <strike>strikethrough</strike>, and <a href="TODO">link</a> for inline elements.{" "}
-								Of course, elements can be <em><strong>nested</strong></em> if that’s your thing.
-							</p>
-							<p>
-								<br />
-							</p>
-							<p>
-								– Added basic support for shortcuts.{" "}
-								You can use <code>{ctrlOrCmd}-i</code> for <em>italics</em>, <code>{ctrlOrCmd}-b</code> for <strong>bold</strong>, <code>shift-{ctrlOrCmd}-c</code> for <code>code</code>, <code>shift-{ctrlOrCmd}-x</code> for <strike>strikethrough</strike>, and <code>{ctrlOrCmd}-k</code> for <a href="TODO">links</a>.{" "}
-								Finally, you can use <code>shift-{ctrlOrCmd}-p</code> to <em>remove</em> formatting from a selection.
-							</p>
-							<p>
-								<br />
-							</p>
-							<p>
-								*Note that formatting without a selection is not yet supported.
-							</p>
-						</ReleaseItem>
-					</React.Fragment>
-
+					<Releases />
 				) : (
-
 					<div className="p-6">
 						<span className="inline-block">
 							<pre className="whitespace-pre-wrap break-words font-mono text-gray-800 subpixel-antialiased" style={{ MozTabSize: 2, tabSize: 2, fontSize: "0.8125rem", lineHeight: 1.4375 }}>
@@ -124,7 +66,6 @@ const Output = React.forwardRef(({ output, setOutput }, forwardedRef) => {
 							</pre>
 						</span>
 					</div>
-
 				)}
 			</div>
 		</Transition>
@@ -185,6 +126,7 @@ const FixedPreferences = ({ state, dispatch }) => {
 		<div className="px-3 pb-4 fixed inset-0 flex flex-col items-end pointer-events-none">
 			<div className="py-2 flex flex-row justify-between">
 
+				{/* Changelog */}
 				<button
 					className="p-2 text-gray-400 hover:text-gray-800 hover:bg-gray-100 focus:bg-gray-100 rounded-full focus:outline-none transition duration-200 ease-in-out pointer-events-auto"
 					style={{ color: output.show && output.detail === "changelog" && "var(--gray-800)" }}
@@ -195,6 +137,7 @@ const FixedPreferences = ({ state, dispatch }) => {
 					</svg>
 				</button>
 
+				{/* GFM */}
 				<button
 					className="p-2 text-gray-400 hover:text-gray-800 hover:bg-gray-100 focus:bg-gray-100 rounded-full focus:outline-none transition duration-200 ease-in-out pointer-events-auto"
 					style={{ color: output.show && output.detail === "gfm" && "var(--gray-800)" }}
@@ -205,6 +148,7 @@ const FixedPreferences = ({ state, dispatch }) => {
 					</svg>
 				</button>
 
+				{/* HTML */}
 				<button
 					className="p-2 text-gray-400 hover:text-gray-800 hover:bg-gray-100 focus:bg-gray-100 rounded-full focus:outline-none transition duration-200 ease-in-out pointer-events-auto"
 					style={{ color: output.show && output.detail === "html" && "var(--gray-800)" }}
---
+++ src/EditorApp/Releases.js
@@ -0,0 +1,63 @@
+import ctrlOrCmd from "lib/Client/ctrlOrCmd"
+import React from "react"
+import { ReadOnlyEditor } from "Editor"
+
+const ReleaseItem = ({ date, release, children }) => (
+	<div className="px-6 py-4">
+		<div className="pb-2 flex flex-row justify-end">
+			<div className="px-2.5 py-0.5 bg-green-100 rounded-full">
+				<p className="font-medium text-xs leading-4 text-green-800">
+					{date} &nbsp;– &nbsp;v{release}
+				</p>
+			</div>
+		</div>
+		<ReadOnlyEditor className="text-gray-800" style={{ fontSize: "0.9375rem" }}>
+			{children}
+		</ReadOnlyEditor>
+	</div>
+)
+
+const Releases = () => (
+	<React.Fragment>
+		<ReleaseItem date="July 22, 2020" release="0.3">
+			<p>
+				– Added a dedicated changelog and improved readability for markdown and HTML-rendered output.
+			</p>
+		</ReleaseItem>
+		<hr className="border-t border-gray-100" />
+		<ReleaseItem date="July 21, 2020" release="0.2">
+			<p>
+				– Added basic support for resolving to plaintext, GitHub-Flavored Markdown, and HTML.
+			</p>
+		</ReleaseItem>
+		<hr className="border-t border-gray-100" />
+		<ReleaseItem date="July 20, 2020" release="0.1">
+			<p>
+				– Added complete support for all backspace operations, such as <code>"backspace-rune"</code>, <code>"backspace-word"</code>, and <code>"backspace-line"</code>.
+			</p>
+			<p>
+				<br />
+			</p>
+			<p>
+				– Added basic support for <em>italics</em>, <strong>bold</strong>, <code>code</code>, <strike>strikethrough</strike>, and <a href="TODO">link</a> for inline elements.{" "}
+				Of course, elements can be <em><strong>nested</strong></em> if that’s your thing.
+			</p>
+			<p>
+				<br />
+			</p>
+			<p>
+				– Added basic support for shortcuts.{" "}
+				You can use <code>{ctrlOrCmd}-i</code> for <em>italics</em>, <code>{ctrlOrCmd}-b</code> for <strong>bold</strong>, <code>shift-{ctrlOrCmd}-c</code> for <code>code</code>, <code>shift-{ctrlOrCmd}-x</code> for <strike>strikethrough</strike>, and <code>{ctrlOrCmd}-k</code> for <a href="TODO">links</a>.{" "}
+				Finally, you can use <code>shift-{ctrlOrCmd}-p</code> to <em>remove</em> formatting from a selection.
+			</p>
+			<p>
+				<br />
+			</p>
+			<p>
+				*Note that formatting without a selection is not yet supported.
+			</p>
+		</ReleaseItem>
+	</React.Fragment>
+)
+
+export default Releases
