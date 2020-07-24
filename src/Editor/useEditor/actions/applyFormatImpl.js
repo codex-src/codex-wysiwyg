2cff7f9dd60f55b5ba125cc19faa923285f28599
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-07-25 04:47:58 +0900

    Cleaned up applyFormatImpl implementation

--- src/Editor/useEditor/actions/applyFormatImpl.js
+++ src/Editor/useEditor/actions/applyFormatImpl.js
@@ -1,64 +1,54 @@
 import findIndex from "../../utils/findIndex"
 import { rangeIsCollapsed } from "../../types/Range"
 
-// Tests for:
-//
-// - "plaintext"
-// - "should-not-apply"
-// - "should-apply"
-//
-function testShouldApply(formatType, children) {
-	if (formatType === "plaintext") {
-		return "plaintext"
-	}
-	const every = children.every(each => each.types[formatType] !== undefined)
-	const shouldApply = ({
-		false: "should-apply",    // Not every -> "should-apply"
-		true: "should-not-apply", // Every -> "should-not-apply"
-	})[every]
-	return shouldApply
-}
-
-// Gets the current children. Note that elements is expected
-// to be a subsection of elements.
-function getChildrenFromRange(elements, range) {
+// Queries children from a range.
+function queryChildrenFromRange(elements, range) {
 	const children = []
 	for (const each of elements) {
-		// Start offset:
-		let t1 = 0
+		let x1 = 0
 		if (each.key === range.start.key) {
-			t1 = findIndex(each.props.children, range.start.offset)
+			x1 = findIndex(each.props.children, range.start.offset)
 		}
-		// End offset:
-		let t2 = each.props.children.length
+		let x2 = each.props.children.length
 		if (each.key === range.end.key) {
-			t2 = findIndex(each.props.children, range.end.offset)
+			x2 = findIndex(each.props.children, range.end.offset)
 		}
-		children.push(...each.props.children.slice(t1, t2))
+		children.push(...each.props.children.slice(x1, x2))
 	}
 	return children
 }
 
+// Tests whether to apply a format. Returns "plaintext",
+// "should-not-apply", or "should-apply".
+function testShouldApply(formatType, children) {
+	if (formatType === "plaintext") {
+		return "plaintext"
+	}
+	const didApply = children.every(each => each.types[formatType] !== undefined)
+	return !didApply ? "should-apply" : "should-not-apply"
+}
+
 // Applies a format to the current range.
 const applyFormatImpl = e => (formatType, formatProps = {} /* TODO */) => {
-	if (rangeIsCollapsed(e.range)) {
-		e.pendingRange = e.range // TODO
-		return
-	}
-
 	const x1 = e.elements.findIndex(each => each.key === e.range.start.key)
 	let x2 = x1
 	if (!rangeIsCollapsed(e.range)) {
 		x2 = e.elements.findIndex(each => each.key === e.range.end.key)
 	}
 
-	const ch = getChildrenFromRange(e.elements.slice(x1, x2 + 1), e.range)
-	const shouldApply = testShouldApply(formatType, ch)
+	if (rangeIsCollapsed(e.range)) {
+		e.pendingRange = e.range // TODO
+		return
+	}
 
+	const ch = queryChildrenFromRange(e.elements.slice(x1, x2 + 1), e.range)
+	const shouldApply = testShouldApply(formatType, ch)
 	switch (shouldApply) {
 	case "plaintext":
 		for (const each of ch) {
-			each.types = {} // Reset
+			Object.keys(each.types).map(type => {
+				delete each.types[type]
+			})
 		}
 		break
 	case "should-not-apply":
