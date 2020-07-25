34afca2b428770749e0446c00dadeefb7ae2e7de
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-07-26 00:15:57 +0900

    More house keeping

--- src/Editor/useEditor/impl/insertTextImpl.js
+++ src/Editor/useEditor/impl/insertTextImpl.js
@@ -7,22 +7,24 @@ function insertTextImpl(e, text) {
 	const ch = e.elements.find(each => each.key === e.range.start.key)
 		.props.children
 
-	// Get the current text node:
-	const tx = findIndex(ch, e.range.start.offset)
-	let originalTextNode = {
-		types: {},
-		props: {
-			children: "",
-		},
-	}
-	if (tx < ch.length) {
-		originalTextNode = JSONClone(ch[tx])
+	let originalTextNode = null
+
+	let x = findIndex(ch, e.range.start.offset)
+	if (!x) {
+		originalTextNode = {
+			types: {},
+			props: {
+				children: "",
+			},
+		}
+	} else if (x < ch.length) {
+		originalTextNode = JSONClone(ch[x])
 	}
 
 	deleteImpl(e)
 
-	// Push the new text node:
-	const x = findIndex(ch, e.range.start.offset)
+	// NOTE: Do not reuse x; does not work as expected.
+	x = findIndex(ch, e.range.start.offset)
 	ch.splice(x, 0, {
 		...originalTextNode,
 		props: {
