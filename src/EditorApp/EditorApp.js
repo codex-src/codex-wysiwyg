5e7f4ab23eac6f91d058abe5867f35befb7cf3af
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-07-25 19:07:22 +0900

    More EditorApp refactoring

--- src/EditorApp/EditorApp.js
+++ src/EditorApp/EditorApp.js
@@ -1,26 +1,13 @@
-import * as handlers from "lib/x/handlers"
-import ClassName from "lib/x/ClassName"
 import ctrlOrCmd from "lib/Client/ctrlOrCmd"
 import DocumentTitle from "lib/x/DocumentTitle"
 import FixedPreferences from "./FixedPreferences"
 import FixedStatusBars from "./FixedStatusBars"
-import Highlight from "lib/PrismJS/Highlight"
-import keyCodeFor from "lib/Client/keyCodeFor"
 import React from "react"
-import Releases from "./Releases"
-import Transition from "lib/x/Transition"
 
 import { // Unsorted
 	DebouncedElementsContext,
 	DebouncedRangeContext,
-	useDebouncedElements,
-	useDebouncedRange,
 } from "./contexts"
-
-import { // Unsorted
-	resolveGFM,
-	resolveHTML,
-} from "./resolvers"
 
 import { // Unsorted
 	Editor,
@@ -54,44 +41,42 @@ const App = () => {
 	}, [state.range])
 
 	return (
-		<DocumentTitle title="Codex (0.4)">
-			<div className="px-6 py-32 flex flex-row justify-center">
-				<div className="w-full max-w-2xl">
+		<DebouncedElementsContext.Provider value={debouncedElements}>
+			<DebouncedRangeContext.Provider value={debouncedRange}>
+				<DocumentTitle title="Codex (0.4)">
+					<div className="px-6 py-32 flex flex-row justify-center">
+						<div className="w-full max-w-2xl">
 
-					<DebouncedElementsContext.Provider value={debouncedElements}>
-						<DebouncedRangeContext.Provider value={debouncedRange}>
 							<FixedPreferences />
-						</DebouncedRangeContext.Provider>
-					</DebouncedElementsContext.Provider>
 
-					<Editor className="text-lg text-gray-800" state={state} dispatch={dispatch}>
-						<p>
-							This prototype currently supports <em>italics</em>, <strong>bold</strong>, <code>code</code>, <strike>strikethrough</strike>, and <a href="TODO">link</a> for inline elements.{" "}
-							Of course, elements can be <strong><em>nested</em></strong> if that’s your thing.
-						</p>
-						<p>
-							<br />
-						</p>
-						<p>
-							Shortcuts are supported!{" "}
-							You can use <code>{ctrlOrCmd}-i</code> for <em>italics</em>, <code>{ctrlOrCmd}-b</code> for <strong>bold</strong>, <code>shift-{ctrlOrCmd}-c</code> for <code>code</code>, <code>shift-{ctrlOrCmd}-x</code> for <strike>strikethrough</strike>, and <code>{ctrlOrCmd}-k</code> for <a href="TODO">links</a>.{" "}
-							Finally, you can use <code>shift-{ctrlOrCmd}-p</code> to <em>remove</em> formatting from a selection.
-						</p>
-						<p>
-							<br />
-						</p>
-						<p>
-							<strong>Please note that many basic features are not yet implemented!</strong> 😎
-						</p>
-					</Editor>
+							<Editor className="text-lg text-gray-800" state={state} dispatch={dispatch}>
+								<p>
+									This prototype currently supports <em>italics</em>, <strong>bold</strong>, <code>code</code>, <strike>strikethrough</strike>, and <a href="TODO">link</a> for inline elements.{" "}
+									Of course, elements can be <strong><em>nested</em></strong> if that’s your thing.
+								</p>
+								<p>
+									<br />
+								</p>
+								<p>
+									Shortcuts are supported!{" "}
+									You can use <code>{ctrlOrCmd}-i</code> for <em>italics</em>, <code>{ctrlOrCmd}-b</code> for <strong>bold</strong>, <code>shift-{ctrlOrCmd}-c</code> for <code>code</code>, <code>shift-{ctrlOrCmd}-x</code> for <strike>strikethrough</strike>, and <code>{ctrlOrCmd}-k</code> for <a href="TODO">links</a>.{" "}
+									Finally, you can use <code>shift-{ctrlOrCmd}-p</code> to <em>remove</em> formatting from a selection.
+								</p>
+								<p>
+									<br />
+								</p>
+								<p>
+									<strong>Please note that many basic features are not yet implemented!</strong> 😎
+								</p>
+							</Editor>
 
-					{/* <FixedStatusBars */}
-					{/* 	state={state} */}
-					{/* /> */}
+							<FixedStatusBars />
 
-				</div>
-			</div>
-		</DocumentTitle>
+						</div>
+					</div>
+				</DocumentTitle>
+			</DebouncedRangeContext.Provider>
+		</DebouncedElementsContext.Provider>
 	)
 }
 
