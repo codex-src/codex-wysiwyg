22c66ca16f5f5558b092735f66152db847154ecd
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-07-28 22:25:42 +0900

    Deprecated must

--- src/lib/x/must/index.js
+++
@@ -1,9 +0,0 @@
-// Throws on undefined, false, -1, "", null values.
-function must(value) {
-	if (value === undefined || value === false || value === -1 || value === "" || value === null) {
-		throw new Error(`must: value assertion; value=${value}`)
-	}
-	return value
-}
-
-export default must
