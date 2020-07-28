1efde665671d7d49924a033b6a3b44c9b501cac7
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-07-29 03:59:06 +0900

    Simplified Tailwind build-step and moved some CSS files out of stylesheets/tailwind-css

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import React from "react"
 import ReactDOM from "react-dom"
 
 import "debug.css"
+import "stylesheets/color-vars.css"
+import "stylesheets/custom.css"
 
 import "stylesheets/tailwind-css/tailwind.generated.css" // Takes precedence
-import "stylesheets/tailwind-css/color-vars.css"
 import "stylesheets/tailwind-css/group-hover.css"
 import "stylesheets/tailwind-css/em-context.css"
 import "stylesheets/tailwind-css/em-context-extended.css"
 
-// import "stylesheets/prism-js/prism-custom-theme.css"
 import "stylesheets/prism-js/language-gfm.css"
 import "stylesheets/prism-js/language-html.css"
 
