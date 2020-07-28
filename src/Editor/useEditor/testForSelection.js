df857c6bdd9d46a3f9d2065be085dc532c6e7402
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-07-28 22:20:24 +0900

    Removed must from testForSelection

--- src/Editor/useEditor/testForSelection.js
+++ src/Editor/useEditor/testForSelection.js
@@ -1,10 +1,6 @@
-import must from "lib/x/must"
-
-// Tests for a selection. A selection can mean range keys
-// and or offsets are different.
+// Tests for the presence of a selection. A selection means
+// range keys and or offsets are different.
 function testForSelection(state) {
-	must(typeof state === "object" && typeof state.range === "object") // TODO: Remove
-
 	const ok = (
 		state.range.start.key !== state.range.end.key ||
 		state.range.start.offset !== state.range.end.offset
