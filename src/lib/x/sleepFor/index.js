e65931d2ac918f5a85327291d8c0f362fe5e945f
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-07-28 19:20:00 +0900

    Deprecated sleepFor

--- src/lib/x/sleepFor/index.js
+++
@@ -1,8 +0,0 @@
-// Sleeps for n-milliseconds.
-//
-// https://stackoverflow.com/a/39914235
-async function sleepFor(timeoutMs) {
-	await new Promise(r => setTimeout(r, timeoutMs))
-}
-
-export default sleepFor
