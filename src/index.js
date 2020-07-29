6055763ca07e186eaf60404e0e3112f900c4bf50
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-07-29 22:41:32 +0900

    More simplification of Tailwind dependencies

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import React from "react"
 import ReactDOM from "react-dom"
 
 import "debug.css"
-import "stylesheets/color-vars.css"
-import "stylesheets/custom.css"
+
+// import "stylesheets/tailwind-css/group-hover.css"
 
 import "stylesheets/tailwind-css/tailwind.generated.css" // Takes precedence
+import "stylesheets/tailwind-css/color-vars.css"
+import "stylesheets/tailwind-css/custom.css"
 import "stylesheets/tailwind-css/em-context.css"
-import "stylesheets/tailwind-css/group-hover.css"
 
 import "stylesheets/prism-js/language-gfm.css"
 import "stylesheets/prism-js/language-html.css"
