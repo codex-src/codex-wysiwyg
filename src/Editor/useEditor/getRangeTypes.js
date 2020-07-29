505ed3aac5c889bc480f46d0c979a569c3767cce
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-07-29 18:28:32 +0900

    Extracted WYSIWYGMenu, started work on getRangeTypes to support many paragraph selections

--- src/Editor/useEditor/getRangeTypes.js
+++ src/Editor/useEditor/getRangeTypes.js
@@ -1,34 +1,63 @@
-import getIndexAtOffset from "./getIndexAtOffset"
+import getIndexAtOffset from "./getIndexAtOffset" // TODO
 import getShorthandVars from "./getShorthandVars"
 import JSONClone from "lib/JSON/JSONClone"
 import JSONEqual from "lib/JSON/JSONEqual"
 import testForSelection from "./testForSelection"
 
+function getNextIndexAtOffset(children, offset) {
+	if (!offset) {
+		return 0
+	}
+	return getIndexAtOffset(children, offset) + 1
+}
+
+// Gets text nodes from an array of elements.
+function getTextNodes(elements, range) {
+	const ch = []
+	for (const each of elements) {
+		if (!each.props.children.length) {
+			// No-op
+			continue
+		}
+		let x1 = 0
+		if (each.key === range.start.key) {
+			x1 = getNextIndexAtOffset(each.props.children, range.start.offset)
+		}
+		let x2 = each.props.children.length
+		if (each.key === range.end.key) {
+			x2 = getNextIndexAtOffset(each.props.children, range.end.offset)
+		}
+		// if (x1 === x2) {
+		// 	x2++
+		// }
+		ch.push(...each.props.children.slice(x1, x2 + (x1 === x2)))
+	}
+	return ch
+}
+
 // Gets the current range types.
 function getRangeTypes(e) {
-	if (e.range.start.key !== e.range.end.key) {
+	const { x1, x2, ch1 } = getShorthandVars(e)
+	if (!testForSelection(e)) {
+		if (!ch1.length) {
+			return {}
+		}
+		const x = getIndexAtOffset(ch1, e.range.start.offset)
+		return ch1[x].types
+	}
+	const ch = getTextNodes(e.elements.slice(x1, x2 + 1), e.range)
+	if (!ch.length) {
 		return {}
 	}
-
-	const { ch1 } = getShorthandVars(e)
-	if (!ch1.length) {
-		return {}
-	}
-
-	const x1 = getIndexAtOffset(ch1, e.range.start.offset + testForSelection(e))
-	const x2 = getIndexAtOffset(ch1, e.range.end.offset)
-
-	const clonedTypes = JSONClone(ch1[x1].types)
+	const clonedTypes = JSONClone(ch[0].types)
 	const clonedTypesKeys = Object.keys(clonedTypes)
-
-	for (const textNode of ch1.slice(x1 + 1, x2 + 1)) { // Uses x1 + 1 to step over clonedTypes
+	for (const textNode of ch.slice(1)) { // Step over clonedTypes
 		for (const key of clonedTypesKeys) {
 			if (!textNode.types[key] || !JSONEqual(textNode.types[key], clonedTypes[key])) {
 				delete clonedTypes[key]
 			}
 		}
 	}
-
 	return clonedTypes
 }
 
--- src/EditorApp/EditorApp.js
+++ src/EditorApp/EditorApp.js
@@ -2,7 +2,7 @@ import ctrlOrCmd from "lib/Client/ctrlOrCmd"
 import MemoFixedPreferences from "./MemoFixedPreferences"
 import React from "react"
 import Transition from "lib/x/Transition"
-import userAgent from "lib/Client/userAgent"
+import WYSIWYGMenu from "./WYSIWYGMenu"
 
 import { // Unsorted
 	ReadOnlyModeSetState,
@@ -20,8 +20,6 @@ const App = () => {
 
 	const [readOnlyMode, setReadOnlyMode] = React.useState(false)
 	const [debouncedElements, setDebouncedElements] = React.useState(() => state.elements)
-
-	const [hoverTooltip, setHoverTooltip] = React.useState("")
 
 	React.useEffect(() => {
 		const id = setTimeout(() => {
@@ -86,179 +84,12 @@ const App = () => {
 							<div className="px-3 py-8 fixed inset-x-0 bottom-0 !opacity-0 pointer-events-none">
 								<div className="flex flex-row justify-center">
 									<div className="pointer-events-auto">
-										<div className="px-3 py-2 flex flex-row items-center bg-white rounded-lg shadow-hero">
 
-											<button
-												className="mx-0.5 p-2 relative text-gray-400 hover:text-gray-800 focus:text-gray-800 hover:bg-gray-100 focus:bg-gray-100 rounded-lg focus:outline-none transition duration-200 ease-in-out pointer-events-auto"
-												style={{
-													color: state.rangeTypes.strong && "var(--blue-500)",
-													backgroundColor: state.rangeTypes.strong && "#ebf5ffbf",
-												}}
-												onFocus={e => setHoverTooltip("bold")}
-												onBlur={e => setHoverTooltip("")}
-												onMouseEnter={e => setHoverTooltip("bold")}
-												onMouseLeave={e => setHoverTooltip("")}
-												onClick={e => {
-													dispatch({
-														type: "ADD_OR_REMOVE_TYPES",
-														types: { strong: {} },
-													})
-												}}
-											>
-												{hoverTooltip === "bold" && (
-													<div className="absolute bottom-full" style={{ left: "50%" }}>
-														<div className="px-2 py-1 relative bg-gray-800 rounded shadow" style={{ left: "-50%" }}>
-															<div className="absolute bottom-0 inset-x-0 flex flex-row justify-center">
-																<div className="-mb-0.5 w-2 h-2 bg-gray-800 rounded-sm shadow transform rotate-45" />
-															</div>
-															<p className="whitespace-pre text-xs text-gray-100">
-																Bold ({!userAgent.MacOSX ? "Ctrl+B" : "⌘+B"})
-															</p>
-														</div>
-													</div>
-												)}
-												<svg className="w-5 h-5" fill="currentColor" viewBox="0 0 16 16">
-													<path d="M4 2a1 1 0 00-1 1v10a1 1 0 001 1h5.5a3.5 3.5 0 001.852-6.47A3.5 3.5 0 008.5 2H4zm4.5 5a1.5 1.5 0 100-3H5v3h3.5zM5 9v3h4.5a1.5 1.5 0 000-3H5z" fillRule="evenodd" />
-												</svg>
-											</button>
+										<WYSIWYGMenu
+											state={state}
+											dispatch={dispatch}
+										/>
 
-											<button
-												className="mx-0.5 p-2 relative text-gray-400 hover:text-gray-800 focus:text-gray-800 hover:bg-gray-100 focus:bg-gray-100 rounded-lg focus:outline-none transition duration-200 ease-in-out pointer-events-auto"
-												style={{
-													color: state.rangeTypes.em && "var(--blue-500)",
-													backgroundColor: state.rangeTypes.em && "#ebf5ffbf",
-												}}
-												onFocus={e => setHoverTooltip("italics")}
-												onBlur={e => setHoverTooltip("")}
-												onMouseEnter={e => setHoverTooltip("italics")}
-												onMouseLeave={e => setHoverTooltip("")}
-												onClick={e => {
-													dispatch({
-														type: "ADD_OR_REMOVE_TYPES",
-														types: { em: {} },
-													})
-												}}
-											>
-												{hoverTooltip === "italics" && (
-													<div className="absolute bottom-full" style={{ left: "50%" }}>
-														<div className="px-2 py-1 relative bg-gray-800 rounded shadow" style={{ left: "-50%" }}>
-															<div className="absolute bottom-0 inset-x-0 flex flex-row justify-center">
-																<div className="-mb-0.5 w-2 h-2 bg-gray-800 rounded-sm shadow transform rotate-45" />
-															</div>
-															<p className="whitespace-pre text-xs text-gray-100">
-																Italic ({!userAgent.MacOSX ? "Ctrl+I" : "⌘+I"})
-															</p>
-														</div>
-													</div>
-												)}
-												<svg className="w-5 h-5" fill="currentColor" viewBox="0 0 16 16">
-													<path d="M6 2.75A.75.75 0 016.75 2h6.5a.75.75 0 010 1.5h-2.505l-3.858 9H9.25a.75.75 0 010 1.5h-6.5a.75.75 0 010-1.5h2.505l3.858-9H6.75A.75.75 0 016 2.75z" fillRule="evenodd" />
-												</svg>
-											</button>
-
-											<button
-												className="mx-0.5 p-2 relative text-gray-400 hover:text-gray-800 focus:text-gray-800 hover:bg-gray-100 focus:bg-gray-100 rounded-lg focus:outline-none transition duration-200 ease-in-out pointer-events-auto"
-												style={{
-													color: state.rangeTypes.code && "var(--blue-500)",
-													backgroundColor: state.rangeTypes.code && "#ebf5ffbf",
-												}}
-												onFocus={e => setHoverTooltip("code")}
-												onBlur={e => setHoverTooltip("")}
-												onMouseEnter={e => setHoverTooltip("code")}
-												onMouseLeave={e => setHoverTooltip("")}
-												onClick={e => {
-													dispatch({
-														type: "ADD_OR_REMOVE_TYPES",
-														types: { code: {} },
-													})
-												}}
-											>
-												{hoverTooltip === "code" && (
-													<div className="absolute bottom-full" style={{ left: "50%" }}>
-														<div className="px-2 py-1 relative bg-gray-800 rounded shadow" style={{ left: "-50%" }}>
-															<div className="absolute bottom-0 inset-x-0 flex flex-row justify-center">
-																<div className="-mb-0.5 w-2 h-2 bg-gray-800 rounded-sm shadow transform rotate-45" />
-															</div>
-															<p className="whitespace-pre text-xs text-gray-100">
-																Code ({!userAgent.MacOSX ? "Shift+Ctrl+C" : "Shift+⌘+C"})
-															</p>
-														</div>
-													</div>
-												)}
-												<svg className="w-5 h-5" fill="currentColor" viewBox="0 0 16 16">
-													<path d="M4.72 3.22a.75.75 0 011.06 1.06L2.06 8l3.72 3.72a.75.75 0 11-1.06 1.06L.47 8.53a.75.75 0 010-1.06l4.25-4.25zm6.56 0a.75.75 0 10-1.06 1.06L13.94 8l-3.72 3.72a.75.75 0 101.06 1.06l4.25-4.25a.75.75 0 000-1.06l-4.25-4.25z" fillRule="evenodd" />
-												</svg>
-											</button>
-
-											<button
-												className="mx-0.5 p-2 relative text-gray-400 hover:text-gray-800 focus:text-gray-800 hover:bg-gray-100 focus:bg-gray-100 rounded-lg focus:outline-none transition duration-200 ease-in-out pointer-events-auto"
-												style={{
-													color: state.rangeTypes.strike && "var(--blue-500)",
-													backgroundColor: state.rangeTypes.strike && "#ebf5ffbf",
-												}}
-												onFocus={e => setHoverTooltip("strikethrough")}
-												onBlur={e => setHoverTooltip("")}
-												onMouseEnter={e => setHoverTooltip("strikethrough")}
-												onMouseLeave={e => setHoverTooltip("")}
-												onClick={e => {
-													dispatch({
-														type: "ADD_OR_REMOVE_TYPES",
-														types: { strike: {} },
-													})
-												}}
-											>
-												{hoverTooltip === "strikethrough" && (
-													<div className="absolute bottom-full" style={{ left: "50%" }}>
-														<div className="px-2 py-1 relative bg-gray-800 rounded shadow" style={{ left: "-50%" }}>
-															<div className="absolute bottom-0 inset-x-0 flex flex-row justify-center">
-																<div className="-mb-0.5 w-2 h-2 bg-gray-800 rounded-sm shadow transform rotate-45" />
-															</div>
-															<p className="whitespace-pre text-xs text-gray-100">
-																Strikethrough ({!userAgent.MacOSX ? "Shift+Ctrl+X" : "Shift+⌘+X"})
-															</p>
-														</div>
-													</div>
-												)}
-												<svg className="w-5 h-5" fill="currentColor" viewBox="0 0 16 16">
-													<path d="M3.72 3.72a.75.75 0 011.06 0L8 6.94l3.22-3.22a.75.75 0 111.06 1.06L9.06 8l3.22 3.22a.75.75 0 11-1.06 1.06L8 9.06l-3.22 3.22a.75.75 0 01-1.06-1.06L6.94 8 3.72 4.78a.75.75 0 010-1.06z" fillRule="evenodd" />
-												</svg>
-											</button>
-
-											<button
-												className="mx-0.5 p-2 relative text-gray-400 hover:text-gray-800 focus:text-gray-800 hover:bg-gray-100 focus:bg-gray-100 rounded-lg focus:outline-none transition duration-200 ease-in-out pointer-events-auto"
-												style={{
-													color: state.rangeTypes.a && "var(--blue-500)",
-													backgroundColor: state.rangeTypes.a && "#ebf5ffbf",
-												}}
-												onFocus={e => setHoverTooltip("link")}
-												onBlur={e => setHoverTooltip("")}
-												onMouseEnter={e => setHoverTooltip("link")}
-												onMouseLeave={e => setHoverTooltip("")}
-												onClick={e => {
-													dispatch({
-														type: "ADD_OR_REMOVE_TYPES",
-														types: { a: {} /* TODO */ },
-													})
-												}}
-											>
-												{hoverTooltip === "link" && (
-													<div className="absolute bottom-full" style={{ left: "50%" }}>
-														<div className="px-2 py-1 relative bg-gray-800 rounded shadow" style={{ left: "-50%" }}>
-															<div className="absolute bottom-0 inset-x-0 flex flex-row justify-center">
-																<div className="-mb-0.5 w-2 h-2 bg-gray-800 rounded-sm shadow transform rotate-45" />
-															</div>
-															<p className="whitespace-pre text-xs text-gray-100">
-																Link ({!userAgent.MacOSX ? "Ctrl+K" : "⌘+K"})
-															</p>
-														</div>
-													</div>
-												)}
-												<svg className="w-5 h-5" fill="currentColor" viewBox="0 0 16 16">
-													<path d="M7.775 3.275a.75.75 0 001.06 1.06l1.25-1.25a2 2 0 112.83 2.83l-2.5 2.5a2 2 0 01-2.83 0 .75.75 0 00-1.06 1.06 3.5 3.5 0 004.95 0l2.5-2.5a3.5 3.5 0 00-4.95-4.95l-1.25 1.25zm-4.69 9.64a2 2 0 010-2.83l2.5-2.5a2 2 0 012.83 0 .75.75 0 001.06-1.06 3.5 3.5 0 00-4.95 0l-2.5 2.5a3.5 3.5 0 004.95 4.95l1.25-1.25a.75.75 0 00-1.06-1.06l-1.25 1.25a2 2 0 01-2.83 0z" fillRule="evenodd" />
-												</svg>
-											</button>
-
-										</div>
 									</div>
 								</div>
 							</div>
