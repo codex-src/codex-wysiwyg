c9044736346c15f50826e82c2ebfc131b4cc444b
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-08-04 02:42:06 +0900

    Removed the transition for read-only mode

--- src/EditorApp/MemoFixedBottomWYSIWYGMenu/MemoFixedBottomWYSIWYGMenu.js
+++ src/EditorApp/MemoFixedBottomWYSIWYGMenu/MemoFixedBottomWYSIWYGMenu.js
@@ -1,6 +1,6 @@
+// import Transition from "lib/x/Transition"
 import JSONEqual from "lib/JSON/JSONEqual"
 import React from "react"
-import Transition from "lib/x/Transition"
 import WYSIWYGMenu from "./WYSIWYGMenu"
 
 function areEqual(prev, next) {
@@ -13,12 +13,13 @@ function areEqual(prev, next) {
 }
 
 const MemoFixedBottomWYSIWYGMenu = React.memo(({ readOnlyMode, focused, rangeTypes }) => (
-	<Transition
-		on={!readOnlyMode}
-		className="transition duration-200 ease-in-out"
-		from="opacity-0"
-		to="opacity-100"
-	>
+	// <Transition
+	// 	on={!readOnlyMode}
+	// 	className="transition duration-200 ease-in-out"
+	// 	from="opacity-0"
+	// 	to="opacity-100"
+	// >
+	!readOnlyMode && (
 		<aside className="px-3 py-8 fixed inset-x-0 bottom-0 pointer-events-none">
 			<div className="flex flex-row justify-center">
 				<div className="pointer-events-auto">
@@ -30,7 +31,7 @@ const MemoFixedBottomWYSIWYGMenu = React.memo(({ readOnlyMode, focused, rangeTyp
 				</div>
 			</div>
 		</aside>
-	</Transition>
+	)
 ), areEqual)
 
 export default MemoFixedBottomWYSIWYGMenu
